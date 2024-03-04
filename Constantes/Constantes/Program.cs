c827f5204d560bccddb2904a57a178cc47c028a9
Blackstar84 <carlosalmada84@gmail.com>
2024-03-04 13:34:18 -0300

    'hola'

---
+++ Constantes/Constantes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constantes
+{
+    internal class Program
+    {
+        const double PI = 3.14159265359;
+        const int semanas = 52, meses = 12;
+        const string nacimiento = "14.08.1984";
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi nacimiento fue el: {0}", nacimiento);
+            Console.Read();
+        }
+    }
+}
