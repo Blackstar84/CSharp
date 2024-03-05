92727c9026346f3aea78e3bca522c8b152343dab
Blackstar84 <carlosalmada84@gmail.com>
2024-03-05 14:22:25 -0300

    Se agrega la clase de TryParse

---
+++ Tomar Decisiones/Tomar Decisiones/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tomar_Decisiones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int temperatura = 10;
+
+            //if (temperatura < 10) 
+            //{
+            //    Console.WriteLine("Abrígate!");
+            //}
+
+            //if (temperatura == 10)
+            //{
+            //    Console.WriteLine("Hace 10 grados de temperatura!");
+            //}
+
+            //if (temperatura > 10)
+            //{
+            //    Console.WriteLine("Está cálido!");
+            //}
+
+            Console.WriteLine("Por Favor, ingresa la temperatura actual: ");
+
+            string temperatura = Console.ReadLine();
+            int numTemperatura = int.Parse(temperatura);
+
+
+            if (numTemperatura < 20)
+            {
+                Console.WriteLine("Abrígate!");  
+            }else if(numTemperatura == 20) 
+            {
+                Console.WriteLine("Vístete cómodo");
+            }
+            else
+            {
+                Console.WriteLine("Usa ropa bien liviana");
+            }
+
+            Console.Read();
+
+        }
+    }
+}
