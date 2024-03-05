ad92c22352a91dd693de6e0966466620d915fef6
Blackstar84 <carlosalmada84@gmail.com>
2024-03-05 16:15:51 -0300

    Se agrega hasta la clase de if anidados

---
+++ If Anidados/If Anidados/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Anidados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool esAdmin = false;
+            bool estaRegistrado = true;
+            string nombreDeUsuario = "";
+            Console.WriteLine("Por favor ingrese su nombre de usuario: ");
+            nombreDeUsuario = Console.ReadLine();
+
+            //if (estaRegistrado)
+            //{
+            //    Console.WriteLine("Hola usuario registrado");
+
+            //    if (nombreDeUsuario != "")
+            //    {
+            //        Console.WriteLine("Hola {0}", nombreDeUsuario);
+
+            //        if (nombreDeUsuario.Equals("Admin"))
+            //        {
+            //            Console.WriteLine("Hola Admin!");
+            //        }
+            //    }
+            //}
+
+            if (estaRegistrado && nombreDeUsuario != "" && nombreDeUsuario.Equals("Admin"))
+            {
+                Console.WriteLine("Hola usuario registrado");
+
+                Console.WriteLine("Hola {0}", nombreDeUsuario);
+
+                Console.WriteLine("Hola Admin!");
+            }
+
+            if (esAdmin || estaRegistrado)
+            {
+                Console.WriteLine("Estás logueado!"); 
+            }
+
+            Console.Read();
+        }
+    }
+}
