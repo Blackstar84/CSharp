7a616abdb006de96a73f2b169a96aceab4625750
Blackstar84 <carlosalmada84@gmail.com>
2024-03-06 13:59:03 -0300

    se agrega hasta la clase de for loop

---
+++ For Loop/For Loop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            // Obtener todos los números impares del 0 al 20
+
+            // Manera larga
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+            for (int i = 1; i < 20; i+=2)
+            {
+                Console.WriteLine("{0} es impar", i);
+            }
+
+
+            Console.Read();
+        }
+    }
+}
