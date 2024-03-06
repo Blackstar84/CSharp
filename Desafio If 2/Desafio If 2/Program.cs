fce18992228372e12724bfcb89e860401f751c78
Blackstar84 <carlosalmada84@gmail.com>
2024-03-06 10:04:01 -0300

    se agrega hasta la clase desafio 2 declaraciones if

---
+++ Desafio If 2/Desafio If 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_If_2
+{
+    internal class Program
+    {
+        static int record = 200;
+        static string recordPersona = "Carlos";
+
+        static void Main(string[] args)
+        {
+            RevisarRecord(100, "José");
+            RevisarRecord(210, "Pedro");
+            RevisarRecord(250, "Manuel");
+
+            Console.Read();
+        }
+
+        public static void RevisarRecord(int puntaje, string jugador)
+        {
+            if (puntaje > record) 
+            {
+                record = puntaje;
+                recordPersona = jugador;
+
+                Console.WriteLine("El nuevo record es {0}", puntaje);
+                Console.WriteLine("Fue logrado por {0}", jugador);
+            }else
+            {
+                Console.WriteLine("El record vigente de {0} logrado por {1} no fue superado", record, recordPersona);
+            }
+
+        } 
+    }
+}
