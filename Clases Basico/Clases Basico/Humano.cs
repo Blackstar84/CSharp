967d3370b2726d40e73bf53b7d93c2901a00a9c4
Blackstar84 <carlosalmada84@gmail.com>
2024-03-07 17:04:12 -0300

    se agrega hasta la clase de Objetos nuestra primera clase propia 74

---
+++ Clases Basico/Clases Basico/Humano.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Basico
+{
+    internal class Humano
+    {
+        // Crear un Miembro Variable
+
+        public string primerNombre;
+        public string primerApellido;
+
+        // Miembro Método
+
+        public void presentarme()
+        {
+            Console.WriteLine("Hola soy {0} {1}", primerNombre, primerApellido);
+        }
+
+        
+    }
+}
---
+++ Clases Basico/Clases Basico/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Basico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creamos un objeto desde mi clase
+            // Es una instancia de Humano
+
+            Humano carlos = new Humano();
+
+            // Accedemos a una variable pública externa
+            
+            carlos.primerNombre = "Carlos";
+
+            // Llamamos a un método de la clase 
+
+            carlos.presentarme();
+
+
+            // Creamos una nueva instancia de Humano
+
+            Humano pedro = new Humano();
+
+            pedro.presentarme();
+
+
+
+            Humano christian = new Humano();
+
+            christian.primerNombre = "Christian";
+            christian.primerApellido = "Garay";
+
+            christian.presentarme();
+
+
+
+            Console.Read();
+
+        }
+    }
+}
