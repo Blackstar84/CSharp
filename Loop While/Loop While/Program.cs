aaba94c475f3da5d36d1be65e8ffbc51ab6dfb3c
Blackstar84 <carlosalmada84@gmail.com>
2024-03-07 12:59:39 -0300

    se agrega hasta la clase del bucle while

---
+++ Loop While/Loop While/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+
+            //while (contador < 10)
+            //{
+            //    Console.WriteLine("Número {0}", contador);
+            //    contador++;
+            //}
+
+            string textoIngresado = "";
+
+            while (textoIngresado.Equals(""))
+            {
+                Console.WriteLine("Por favor presione enter cada vez que un alumno ingrese al bus. Para finalizar ingrese otro valor");
+                textoIngresado = Console.ReadLine();
+                contador++;
+                Console.WriteLine("Alumnos en el bus: {0}", contador);
+            }
+
+            Console.Read();
+        }
+    }
+}
