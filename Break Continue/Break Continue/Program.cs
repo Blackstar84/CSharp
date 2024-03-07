b1674b06f6f5fddc8bc849646e8b939be346531c
Blackstar84 <carlosalmada84@gmail.com>
2024-03-07 14:38:23 -0300

    se agrega hasta la clase de break continue

---
+++ Break Continue/Break Continue/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Continue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                
+
+                //if (i == 3)
+                //{
+                //    Console.WriteLine("A los 3 nos detenemos");
+                //    break;
+                //}
+
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("El próximo es impar");
+                    continue;
+                }
+                Console.WriteLine(i);
+
+            }
+
+            Console.Read();
+        }
+    }
+}
