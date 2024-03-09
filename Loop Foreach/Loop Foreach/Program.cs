0ff581ba951e97ed5890eaf27ef5488da282754b
Blackstar84 <carlosalmada84@gmail.com>
2024-03-09 10:32:08 -0300

    se agrega la clase de foreach

--- Loop Foreach/Loop Foreach/Program.cs
+++ Loop Foreach/Loop Foreach/Program.cs
@@ -10,6 +10,33 @@ namespace Loop_Foreach
     {
         static void Main(string[] args)
         {
+            int[]nums = new int[10];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = i;
+            }
+
+            //for (int j = 0;j < nums.Length;j++)
+            //{
+            //    Console.WriteLine("El valor del índice {0} es {1}", j, nums[j]);
+            //}
+
+            foreach (int k in nums)
+            {
+                Console.WriteLine("El valor del índice {0} es {1}", k, nums[k]);
+            }
+
+            string[]amigos = {"Pedro", "Jorgete", "Christian", "Juacados", "Oscar"};
+
+            foreach (string amigo in amigos)
+            {
+                Console.WriteLine("Hola, {0}, te aprecio mucho.", amigo);
+            }
+
+
+
+            Console.Read();
         }
     }
 }
