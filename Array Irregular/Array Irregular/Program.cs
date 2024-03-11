8d0ec7f1011f5923b8b57736c950909f03a0e7c3
Blackstar84 <carlosalmada84@gmail.com>
2024-03-11 17:27:01 -0300

    se agrega hasta la clase array irregulares

---
+++ Array Irregular/Array Irregular/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Irregular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] irregular = new int[3][];
+
+            // Esta es una forma de agregar valores al array irregular
+
+            irregular[0] = new int[5];
+            irregular[1] = new int[3];
+            irregular[2] = new int[2];
+
+            irregular[0] = new int[]
+            {
+                2, 3, 8, 9, 10
+            };
+            irregular[1] = new int[]
+            {
+                23, 100, 15
+            };
+
+            irregular[2] = new int[] 
+            {
+                5, 900
+            };
+
+
+            // Esta es la otra forma de agregar valores al array irregular
+
+            int[][] irregular2 = new int[][]
+            {
+                  new int[] { 2, 3, 8, 9, 10 },
+                  new int[] { 23, 100, 15 },
+                  new int[] { 5, 900 }
+            };
+
+            Console.WriteLine("El valor en el medio del primer array es {0}", irregular2[0][2]);
+            
+
+
+            for (int i = 0; i < irregular2.Length; i++)
+            {
+                Console.WriteLine("Los valores del array {0} son: ", i);
+
+                for(int j = 0; j < irregular2[i].Length; j++)
+                {
+                    Console.WriteLine("{0}", irregular2[i][j]);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
